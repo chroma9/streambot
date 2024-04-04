13020cf45b1b368b4793993442a79a1c86abde43
Chroma9 <officialchroma9@gmail.com>
2024-04-04 09:39:03 -0400

    Create formats.py

---
+++ lib/formats.py
@@ -0,0 +1,31 @@
+import os as OS
+
+default_format = '''
+**[USERNAME]** is now live on Twitch! 
+CCome watch them live: https://www.twitch.tv/[USERNAME]
+'''
+
+class MessageFormat:
+    def __init__(self):
+        self.directory = str('formats')
+        self.formats = {}
+
+        self.formats['default'] = default_format
+
+    def load_formats(self):
+        for file in OS.listdir(self.directory):
+            if file.endswith('.txt'):
+                path = str(f'{self.directory}/{file}')
+                name = file[:-4]
+                self.load_format_file(path, name)
+    
+    def load_format_file(self, path, name):
+        with open(path, 'r') as file:
+            self.formats[name] = file.read()
+    
+    def get_message_format(self, username):
+        if username in self.formats:
+            return self.formats[username]
+        else:
+            return self.formats['default']
+            
