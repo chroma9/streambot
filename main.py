fa20447e4e4939ffd39dc1e1b148611a7734a362
Chroma9 <officialchroma9@gmail.com>
2024-04-04 14:42:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,24 @@ import discord
 from discord.ext import commands
 
 from lib.reporter import Reporter
+from lib.configuration import Configuration
+from lib.twitch import TwitchSystem
+from lib.formats import FormatSystem
 
-# Might find a different way to do this later. Not enough coffee ATM.
+# Might find a different way to do this later.
+# Literally, too early to worry about this shoddy method.
+# It works so I'll leave it for now.
 client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
+config = Configuration()
 report = Reporter()
+twitch = TwitchSystem()
+formats = FormatSystem()
 
 @client.event
 async def on_ready():
+    # Temporary Job
     print(f"Connected as {client.user.id}")
 
 if __name__ == '__main__':
-    pass
+    formats.load_formats()
